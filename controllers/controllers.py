74de38f16c5250c02a042808fa44f28c37b53165
Adrian <a.delrio@ingetive.com>
2017-03-23 14:21:40 +0100

    a

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -5,6 +5,6 @@ from odoo.http import request
 class pruebaIp(http.Controller):
     @http.route('/prueba_ip/', type='http', auth='public', website=True)
     def prueba_ip(self, **kw):
-        ip = request.httprequest.environ['REMOTE_ADDR']
+        ip = request.httprequest.remote_addr
         ip_real = request.httprequest.environ['HTTP_X_REAL_IP']
         return "Mi IP es: " + str(ip) + " <br> Mi IP real: " + ip_real
