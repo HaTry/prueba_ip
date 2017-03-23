d4fc422373900768d10452d69700db7f65b943b1
Adrian <a.delrio@ingetive.com>
2017-03-23 13:21:11 +0100

    guardar

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -6,4 +6,5 @@ class pruebaIp(http.Controller):
     @http.route('/prueba_ip/', type='http', auth='public', website=True)
     def prueba_ip(self, **kw):
         ip = request.httprequest.environ['REMOTE_ADDR']
-        return "Mi IP es: " + str(ip)
+        ip_real = request.httprequest.environ['HTTP_X_REAL_IP']
+        return "Mi IP es: " + str(ip) + " <br> Mi IP real: " + ip_real
